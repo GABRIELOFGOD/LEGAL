e3593e88560b955befe94c146e83ec4b6409c333
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-03-12 17:55:04 +0100

    all

--- components/Join.jsx
+++ components/Join.jsx
@@ -8,9 +8,9 @@ const Join = () => {
         <div className='w-full absolute h-full bg-cover rounded-md ' style={{background: `url(${internet})`, backgroundSize: 'cover'}}></div>
         <div className='z-20 flex flex-col items-center justify-center text-center'>
           <p className="md:text-5xl text-3xl text-white font-bold">Join us to stop gender based violence in our community</p>
-          <div className='flex gap-10 mt-10'>
-            <button className='rounded-md bg-greener text-white py-2.5 px-6'>Join as a volunteer</button>
-            <button className='rounded-md bg-white text-black py-2.5 px-6'>Donate</button>
+          <div className='flex text-sm md:text-xs gap-10 mt-10'>
+            <button className='rounded-md bg-greener text-white py-2 md:py-2.5 px-6'>Join as a volunteer</button>
+            <button className='rounded-md bg-white text-black py-2 md:py-2.5 px-6'>Donate</button>
           </div>
         </div>
       </div>
